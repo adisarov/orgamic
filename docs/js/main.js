53eeb2ec6124d2e6dc55a3ed3eaaa27041621c37
adisarov <you@example.comadisarov.dev@gmail.com>
2022-02-27 19:37:55 +0200

    123

--- docs/js/main.js
+++ docs/js/main.js
@@ -19,5 +19,22 @@ var swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev'
   }
 });
+var ff2 = new Swiper('.ff-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2'
+  }
+});
 /******/ })()
 ;
--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,23 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const ff2 = new Swiper('.ff-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
+});
